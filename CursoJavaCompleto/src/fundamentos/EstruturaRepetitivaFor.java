247322c4e3be1baa5c730f8408e297140a33b34b
Carlos Silva <carlosrnsilva01@gmail.com>
2026-03-28 22:45:37 -0300

    Segunto commit - Estrutura for

---
+++ CursoJavaCompleto/src/fundamentos/EstruturaRepetitivaFor.java
@@ -0,0 +1,41 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+/*
+ estrutura
+    for ( inicio; condição; incremento) {
+    comando 1
+    comando 2
+    }
+ */
+
+public class EstruturaRepetitivaFor {
+
+    public static void main(String[] args) {
+        /*
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int soma = 0;
+        //i = i + 1
+        for (int i= 0; i<N; i++ ){
+            int x = sc.nextInt();
+            soma += x;
+        }
+        System.out.println(soma);
+        sc.close();
+
+        // conntagem
+        for (int i=0; i<5; i++) {
+            System.out.println("Valor de i: " + i);
+        }
+ */
+        // conntagem
+
+        for (int i=4; i>=0; i--){
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
