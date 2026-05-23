1495e8c281e203813dd73879880996b12f67caaf
Carlos Silva <carlosrnsilva01@gmail.com>
2026-05-22 22:32:45 -0300

    Versao1 e 2 - MembrosEstaticos

--- CursoJavaCompleto/src/fundamentos/introducaoPOO/Util/Calculator.java
+++ CursoJavaCompleto/src/fundamentos/introducaoPOO/Util/Calculator.java
@@ -3,7 +3,6 @@ package fundamentos.introducaoPOO.Util;
 public class Calculator {
 
     public static final double PI = 3.14159;
-
     public double circuference(double radius){
         return 2.0 * PI * radius;
     }
